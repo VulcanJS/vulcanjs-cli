95a6c030dff2acb1660540ac2f5a18ec463776f1
Kerem Kazan <kerem.kazan@gmail.com>
2017-07-11 13:38:03 -0400

    can now pass options

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const yeoman = require('yeoman-environment');
-const env = yeoman.createEnv();
+const parseArgs = require('minimist');
+// const optionsManager = require('generator-vulcanjs/lib/optionsManager');
+
 const appGenerator = require.resolve('generator-vulcanjs/generators/app');
 const packageGenerator = require.resolve('generator-vulcanjs/generators/package');
 const modelGenerator = require.resolve('generator-vulcanjs/generators/model');
 const componentGenerator = require.resolve('generator-vulcanjs/generators/component');
 const routeGenerator = require.resolve('generator-vulcanjs/generators/route');
 const remover = require.resolve('generator-vulcanjs/generators/remove');
-env.register(app, 'vulcanjs:app');
-const done = () => { console.log('DONE!'); };
-env.run('vulcanjs:app', done);
+
+const env = yeoman.createEnv();
+
+env.register(appGenerator, 'vulcanjs:app');
+env.register(packageGenerator, 'vulcanjs:package');
+env.register(modelGenerator, 'vulcanjs:model');
+env.register(componentGenerator, 'vulcanjs:component');
+env.register(routeGenerator, 'vulcanjs:route');
+env.register(remover, 'vulcanjs:remove');
+
+const options = parseArgs(process.argv.slice(2));
+
+function runWithOptions (generator, callback) {
+  env.run(generator, options, callback);
+}
+
+runWithOptions('vulcanjs:package');
--- node_modules/generator-vulcanjs/generators/model/schema/templates/partials/schemaProperty.js
+++
@@ -1,8 +0,0 @@
-<%- name %> : {
-  <% if (isHidden) { %> hidden: true <% } else { %> label: '<%= label %>' <% } %>,
-  type: <%- type %>,
-  optional: <%- isOptional %>,
-  viewableBy: <%- viewableBy %>,
-  insertableBy: <%- insertableBy %>,
-  editableBy: <%- editableBy %>,
-},
