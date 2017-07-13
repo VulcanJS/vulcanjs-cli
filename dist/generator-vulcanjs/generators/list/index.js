1fb7fa51384d75d6b708abcc95ca727447f0c02a
Kerem Kazan <kerem.kazan@gmail.com>
2017-07-13 13:53:20 -0400

    added grunt scripts

---
+++ dist/generator-vulcanjs/generators/list/index.js
@@ -0,0 +1,30 @@
+const VulcanGenerator = require('../../lib/VulcanGenerator');
+
+module.exports = class extends VulcanGenerator {
+  initializing() {
+    this._assert('isVulcan');
+  }
+
+  _registerArguments() {
+    this._registerOptions('vulcanjsListableComponent');
+  }
+
+  prompting() {
+    if (!this._canPrompt()) {
+      return false;
+    }
+    // const questions = this._getQuestions(
+    //   'vulcanjsListableComponent'
+    // );
+    const questions = [];
+    return this.prompt(questions).then(answers => {
+      this.props = {
+        vulcanjsListableComponent: this._finalize('raw', 'vulcanjsListableComponent', answers)
+      };
+    });
+  }
+
+  end() {
+    this._end();
+  }
+};
